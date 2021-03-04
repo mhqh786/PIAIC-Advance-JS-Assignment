a3041f77425f108548f19a3217fa8fd82e0b3515
mhqh786 <mhqh77@gmail.com>
2021-03-04 11:39:22 +0500

    2nd Assignment

---
+++ 02 Template Literals/main.js
@@ -0,0 +1,4 @@
+var firstName = "Muhammad Hassan Qureshi"
+var designation = "Advocate High Court"
+var description = `My Name is ${firstName}, i am ${designation} at Multan Bench.`
+console.log(description)
