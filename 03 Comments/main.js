d022e13de894b00dc7d46d60fa4a6bb6b1e6b415
mhqh786 <mhqh77@gmail.com>
2021-03-04 11:44:03 +0500

    3rd Assignment

---
+++ 03 Comments/main.js
@@ -0,0 +1,13 @@
+// print name and designation in console
+var firstName = "Muhammad Hassan Qureshi"
+var designation = "Advocate High Court"
+var description = `My Name is ${firstName}, i am ${designation} at Multan Bench.`
+console.log(description)
+
+// you will see multiline codes here:
+/*
+var firstName = "Muhammad Hassan Qureshi"
+var designation = "Advocate High Court"
+var description = `My Name is ${firstName}, i am ${designation} at Multan Bench.`
+console.log(description)
+*/
