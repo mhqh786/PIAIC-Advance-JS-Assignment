ca5a6037ea41c4533e6bbfdeddd744ef8b2a8797
mhqh786 <mhqh77@gmail.com>
2021-03-21 20:01:58 +0500

    adding assignment No. 5

---
+++ 05_Hoisting/index.js
@@ -0,0 +1,15 @@
+function myname(name){
+    document.write("My name is " + name + "<br>");
+}
+myname("Muhammad Hassan");
+
+// -------------------------------
+
+myname1("Mr. Muhammad Hassan Qureshi");
+function myname1(name1){
+    document.write("My Real name is " + name1);
+}
+
+// --------------------------------
+
+
